c5c3f511be91a28506c9358f125db7c65ee62425
ibjc <2389015+ibjc@users.noreply.github.com>
2022-12-13 10:10:43 +0000

    it flag to be ssh-able

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --name pybc-relayer -p 6994:6994 -v /path/to/repos:/repos pybc-relayer:latest
+docker run -it -d --name pybc-relayer -p 6994:6994 -v /path/to/repos:/repos pybc-relayer:latest
